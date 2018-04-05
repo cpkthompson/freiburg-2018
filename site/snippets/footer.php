b05c8dfb7272e3cc1a32421d73c8e9580057e72e
Markus Seyfferth <markus.seyfferth@smashingmagazine.com>
2018-04-05 14:58:40 +0200

    scripts & sponsors

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -18,10 +18,26 @@
     </nav>
   </div>
 </footer>
-<script>/*! loadCSS: load a CSS file asynchronously. [c]2018 @scottjehl, Filament Group, Inc. Licensed MIT */(function(a){var b=function(c,k,d){var l=a.document;var m=l.createElement("link");var e;if(k){e=k}else{var i=(l.body||l.getElementsByTagName("head")[0]).childNodes;e=i[i.length-1]}var f=l.styleSheets;m.rel="stylesheet";m.href=c;m.media="only x";function j(n){if(l.body){return n()}setTimeout(function(){j(n)})}j(function(){e.parentNode.insertBefore(m,(k?e:e.nextSibling))});var h=function(o){var n=m.href;var p=f.length;while(p--){if(f[p].href===n){return o()}}setTimeout(function(){h(o)})};function g(){if(m.addEventListener){m.removeEventListener("load",g)}m.media=d||"all"}if(m.addEventListener){m.addEventListener("load",g)}m.onloadcssdefined=h;h(g);return m};if(typeof exports!=="undefined"){exports.loadCSS=b}else{a.loadCSS=b}}(typeof global!=="undefined"?global:this));loadCSS( "/assets/styles/fr17.min.css" );</script>
-<script>/*! onloadCSS: adds onload support for asynchronous stylesheets loaded with loadCSS. [c]2018 @zachleat, Filament Group, Inc. Licensed MIT */;function onloadCSS(b,d){var c;function a(){if(!c&&d){c=true;d.call(b)}}if(b.addEventListener){b.addEventListener("load",a)}if(b.attachEvent){b.attachEvent("onload",a)}if("isApplicationInstalled" in navigator&&"onloadcssdefined" in b){b.onloadcssdefined(a)}};</script>
-<script>/*! CSS rel=preload polyfill. Depends on loadCSS function. [c]2018 @scottjehl, Filament Group, Inc. Licensed MIT  */(function(a){if(!a.loadCSS){return}var b=loadCSS.relpreload={};b.support=function(){try{return a.document.createElement("link").relList.supports("preload")}catch(d){return false}};b.poly=function(){var d=a.document.getElementsByTagName("link");for(var e=0;e<d.length;e++){var f=d[e];if(f.rel==="preload"&&f.getAttribute("as")==="style"){a.loadCSS(f.href,f);f.rel=null}}};if(!b.support()){b.poly();var c=a.setInterval(b.poly,300);if(a.addEventListener){a.addEventListener("load",function(){a.clearInterval(c)})}if(a.attachEvent){a.attachEvent("onload",function(){a.clearInterval(c)})}}}(this));</script>
-<script>(function(a){var b=function(f,c){var e=a.document.getElementsByTagName("script")[0];var d=a.document.createElement("script");d.src=f;d.async=true;e.parentNode.insertBefore(d,e);if(c&&typeof(c)==="function"){d.onload=c}return d};if(typeof module!=="undefined"){module.exports=b}else{a.loadJS=b}}(typeof global!=="undefined"?global:this));loadJS('//static.getclicky.com/js');loadJS('https://smashingconf.com/assets/scripts/j.js');</script>
-<script type="text/javascript">try{ clicky.init(66590379); }catch(e){}</script>
+<script>/*! loadCSS: load a CSS file asynchronously. [c]2016 @scottjehl, Filament Group, Inc. Licensed MIT */(function(a){var b=function(c,k,d){var l=a.document;var m=l.createElement("link");var e;if(k){e=k}else{var i=(l.body||l.getElementsByTagName("head")[0]).childNodes;e=i[i.length-1]}var f=l.styleSheets;m.rel="stylesheet";m.href=c;m.media="only x";function j(n){if(l.body){return n()}setTimeout(function(){j(n)})}j(function(){e.parentNode.insertBefore(m,(k?e:e.nextSibling))});var h=function(o){var n=m.href;var p=f.length;while(p--){if(f[p].href===n){return o()}}setTimeout(function(){h(o)})};function g(){if(m.addEventListener){m.removeEventListener("load",g)}m.media=d||"all"}if(m.addEventListener){m.addEventListener("load",g)}m.onloadcssdefined=h;h(g);return m};if(typeof exports!=="undefined"){exports.loadCSS=b}else{a.loadCSS=b}}(typeof global!=="undefined"?global:this));loadCSS( '/sf-2018/assets/styles/sf18.min.css' );</script>
+<script>/*! onloadCSS: adds onload support for asynchronous stylesheets loaded with loadCSS. [c]2016 @zachleat, Filament Group, Inc. Licensed MIT */;function onloadCSS(b,d){var c;function a(){if(!c&&d){c=true;d.call(b)}}if(b.addEventListener){b.addEventListener("load",a)}if(b.attachEvent){b.attachEvent("onload",a)}if("isApplicationInstalled" in navigator&&"onloadcssdefined" in b){b.onloadcssdefined(a)}};</script>
+<script>/*! CSS rel=preload polyfill. Depends on loadCSS function. [c]2016 @scottjehl, Filament Group, Inc. Licensed MIT  */(function(a){if(!a.loadCSS){return}var b=loadCSS.relpreload={};b.support=function(){try{return a.document.createElement("link").relList.supports("preload")}catch(d){return false}};b.poly=function(){var d=a.document.getElementsByTagName("link");for(var e=0;e<d.length;e++){var f=d[e];if(f.rel==="preload"&&f.getAttribute("as")==="style"){a.loadCSS(f.href,f);f.rel=null}}};if(!b.support()){b.poly();var c=a.setInterval(b.poly,300);if(a.addEventListener){a.addEventListener("load",function(){a.clearInterval(c)})}if(a.attachEvent){a.attachEvent("onload",function(){a.clearInterval(c)})}}}(this));</script>
+<script>(function(a){var b=function(f,c){var e=a.document.getElementsByTagName("script")[0];var d=a.document.createElement("script");d.src=f;d.async=true;e.parentNode.insertBefore(d,e);if(c&&typeof(c)==="function"){d.onload=c}return d};if(typeof module!=="undefined"){module.exports=b}else{a.loadJS=b}}(typeof global!=="undefined"?global:this));loadJS('/assets/scripts/j.js');</script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-60521237-1', 'auto', {'allowLinker': true});
+  ga('require', 'linker');
+  ga('linker:autoLink', ['smashingmagazine.com', 'smashingconf.com'] );
+  ga('set', 'anonymizeIp', true);
+  ga('send', 'pageview');
+
+</script>
+
+<link rel="prefetch" href="https://js.tito.io/v1">
+<link rel="prerender" href="https://js.tito.io/v1">
+
 </body>
 </html>
