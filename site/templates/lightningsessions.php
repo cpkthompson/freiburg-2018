79b6a138ab13cc6f7bcfdaddf7c4d0c77790494a
Charis Rooda <charis@imakewebsites.nl>
2018-07-23 14:33:06 +0800

    Tweaks

--- site/templates/lightningsessions.php
+++ site/templates/lightningsessions.php
@@ -2,12 +2,12 @@
 <?php snippet('menu') ?>
 <section class="c default">
   <h1><?php echo html($page->title()) ?></h1>
-  <?php if ($page->intro() != ''): ?>
+  <?php if ($page->intro() != '') : ?>
     <div class="io">
       <?php echo kirbytext($page->intro()) ?>
     </div>
   <?php endif ?>
-  <?php if ($page->text() != ''): ?>
+  <?php if ($page->text() != '') : ?>
     <div class="tx">
       <?php echo kirbytext($page->text()) ?>
     </div>
